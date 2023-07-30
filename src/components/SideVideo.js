9db40183da4f7732fdbc0647871af22b41bddcc7
Prajjwal98Dubey <110735413+Prajjwal98Dubey@users.noreply.github.com>
2023-07-31 01:28:04 +0530

    Update SideVideo.js

--- src/components/SideVideo.js
+++ src/components/SideVideo.js
@@ -7,7 +7,7 @@ const SideVideo = () => {
 
       useEffect(()=>{
         const getVideos=async()=>{
-            const data=await fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyBx3O0o7OWUS816Jov7sS5JAXeX-1x0HhQ")
+            const data=await fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=[YOUR_API_KEY]")
             const json=await data.json()
             setVideos(json.items)
             setIsLoading(false)
